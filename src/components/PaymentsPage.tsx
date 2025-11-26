b819e43f3d2703260f9cd30a8a4807d89699b634
abTaibi98 <139547376+abTaibi98@users.noreply.github.com>
2025-11-26 00:05:12 +0000

    first step

--- src/components/PaymentsPage.tsx
+++ src/components/PaymentsPage.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { Container } from './components'
+import React from 'react';
+import { Container } from './components';
+import { PaymentPageController } from '../pages/PaymentPage/PaymentPage.controller';
 
 export const PaymentsPage = () => {
-  return <Container>Start here</Container>;
+  return (
+    <Container>
+      <PaymentPageController />
+    </Container>
+  );
 };
--- src/types/payment.ts
+++ src/types/payment.ts
@@ -1,5 +1,58 @@
-export interface Payment {
+import { Dispatch, SetStateAction } from 'react';
+
+export interface PaymentSearchQuery {
+  search: string;
+  currency: string;
+  page: number;
+  pageSize: number;
 }
 
-export interface PaymentSearchResponse {
+export interface PaymentData {
+  page: number;
+  pageSize: number;
+  payments: Payment[];
+}
+
+export interface Payment {
+  id?: string;
+  customerName?: string;
+  amount?: number;
+  customerAddress?: string;
+  currency?: Currency;
+  status?: PaymentStatus;
+  date?: string;
+  description?: string;
+}
+
+export enum PaymentStatusEnum {
+  Completed = 'completed',
+  Failed = 'failed',
+  Refunded = 'refunded',
+  Pending = 'pending',
+}
+
+export type PaymentStatus =
+  | PaymentStatusEnum.Completed
+  | PaymentStatusEnum.Failed
+  | PaymentStatusEnum.Pending
+  | PaymentStatusEnum.Refunded;
+
+export type Currency = 'USD' | 'EUR' | 'GBP' | 'AUD' | 'CAD' | 'ZAR' | 'JPY';
+
+export interface PaymentsPageProps {
+  data?: PaymentData;
+}
+export interface Step1TableProps {
+  payments: Payment[];
+}
+
+export interface FetchParams {
+  search: string;
+  currency: Currency | string;
+  page: number;
+  pageSize: number;
+}
+export interface GetPaymentDataProps {
+  fetchParams: FetchParams;
+  setError: Dispatch<SetStateAction<unknown | undefined>>;
 }
